33918aa58635850e164eaef7092f97badfc69456
chowhr1231 <thddbswo14@gmail.com>
2019-11-03 17:57:25 +0900

    데이터 전처리 추가
    받은 데이터를 Model의 입력 형태로 전처리 하는 기능 추가

---
+++ ml/model_by_word/model.py
@@ -0,0 +1,37 @@
+import numpy as np
+from hgtk import text, letter, checker
+
+
+class ModelByWord:
+    def __init__(self):
+        text.decompose = self._decompose
+
+    @staticmethod
+    def _decompose(text, latin_filter=True, compose_code=u" "):
+        result = u""
+
+        for c in list(text):
+            if checker.is_hangul(c):
+
+                if checker.is_jamo(c):
+                    result = result + c + compose_code
+                else:
+                    result = result + "".join(letter.decompose(c)) + compose_code
+
+            else:
+                if latin_filter:  # 한글 외엔 Latin1 범위까지만 포함 (한글+영어)
+                    if checker.is_latin1(c):
+                        result = result + c + compose_code
+                else:
+                    result = result + c + compose_code
+
+        return result
+
+    def _preprocess(self, comment):
+        comment_decompose = text.decompose(comment)
+        result = list(filter(lambda word: word != ' ', comment_decompose.split(' ')))
+        result = list(filter(lambda element: element != '', result))
+        return result
+
+    def fit(self, input):
+        word_list = self._preprocess(input)
