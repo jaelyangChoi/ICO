2652ea9651f99ef421880e8f6ab8e6885b721f48
chowhr1231 <thddbswo14@gmail.com>
2019-11-14 17:34:55 +0900

    Modlue 추가
    request, redirect, url_for module 추가

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 from router import test
 
 app = Flask(__name__, template_folder="templates")
