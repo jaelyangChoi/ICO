09fc3732d365a834c710c3a6451840f789de5158
chowhr1231 <thddbswo14@gmail.com>
2019-11-21 14:55:06 +0900

    connection test를 위한 추가

--- router/test.py
+++ router/test.py
@@ -1,14 +1,16 @@
-from flask import Blueprint, jsonify
-from DB import DAO
+from flask import Blueprint, jsonify, request
+from DB import comment_dao
+
+from ml.model_by_word import model
 
 route_blue = Blueprint('route_blue', __name__)
 
-dao = DAO.DatabaseDAO()
+dao = comment_dao.CommentDAO()
 
 
 @route_blue.route('/test/comments/<url>')
 def select_comments(url):
-    result = dao.selectCommentsByURL(url)
+    result = dao.select_comments_by_url(url)
 
     temp = []
     for data in result:
@@ -16,3 +18,13 @@ def select_comments(url):
         temp.append(data.to_json())
 
     return jsonify(temp)
+
+
+@route_blue.route('/test/ml')
+def ml_comment():
+    comment = request.args.get('comment')
+    ml = model.ModelByWord()
+
+    result = ml.predict(comment)
+
+    return str(result)
