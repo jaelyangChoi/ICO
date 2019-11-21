6ab52970dae0ae67c203d51f8bc0b6e80bf1f688
ChoiJaeLyang <cjl2076@naver.com>
2019-11-21 15:55:12 +0900

    google login

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, Blueprint, request, redirect,url_for
+
 from block import block
 from dao import db_connection
 
@@ -6,6 +7,9 @@ app = Flask(__name__, template_folder="templates")
 # app.register_blueprint(db_connection.db_blue)
 # app.register_blueprint(block.block_blue)
 
+@app.route('/googleCallback')
+def googleCallback():
+    return render_template('index')
 
 @app.route('/')
 def index():
@@ -13,15 +17,29 @@ def index():
 
 
 @app.route('/news')
-def news():
-    return render_template('news1.html')
+def news(form=None):
+    return render_template('news1.html',form=form)
 
 @app.route('/commentInput', methods=['POST'])
 def comment():
-    if request.method == 'POST':
-        form = request.form
+   if request.method == 'POST':
+       form = request.form
+       print(form)
+   return render_template('news1.html', form=form)
 
-    return redirect(url_for('news', form=form))
+# @app.route('/commentInput', methods=['POST'])
+# def comment():
+#     if request.method == 'POST':
+#         form = request.form
+#
+#     return redirect(url_for(news, form=form))
+
+
+@app.route('/keywords', methods=['POST'])
+def keyword():
+    if request.method == 'POST':
+        keywords = request.form['keywords']
+    return render_template('news1.html', keywords=keywords)
 
 
 if __name__ == '__main__':
