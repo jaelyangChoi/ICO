015fa2d95421f76f9504bb7fdcc4e151adb644ce
ChoiJaeLyang <cjl2076@naver.com>
2019-11-26 16:50:33 +0900

    frontserver

--- app.py
+++ app.py
@@ -1,46 +1,56 @@
 from flask import Flask, render_template, Blueprint, request, redirect,url_for
-
+import json
+from router.view import view_blueprint
 from block import block
 from dao import db_connection
 
 app = Flask(__name__, template_folder="templates")
+
+app.register_blueprint(view_blueprint)
 # app.register_blueprint(db_connection.db_blue)
 # app.register_blueprint(block.block_blue)
 
-@app.route('/googleCallback')
-def googleCallback():
-    return render_template('index')
+#@app.route('/googleCallback')
+# @app.route('/')
+# def index():
+#    return render_template('index.html')
 
-@app.route('/')
-def index():
-    return render_template('index')
+@app.route('/news', methods=["GET", "POST"])
+def news():
+    keywords = ['sibal', 'byungsin']
+    keywordList = ', '.join(keywords)
+    # posts = Post.query.all()
+    comments = [{'userID': 'cjl', 'comment': 'aaaaaa\n'}]
+    if request.method == 'POST':
+            new_comment = request.form
+            comments.append(new_comment)
+        # if request.form['keyword']:
+        #     new_keyword = request.form['keywords']
+        #     keywords.append(new_keyword)
 
-
-@app.route('/news')
-def news(form=None):
-    return render_template('news1.html',form=form)
+    return render_template('news1.html', comments=comments, keywords=keywordList)
 
 @app.route('/commentInput', methods=['POST'])
 def comment():
-   if request.method == 'POST':
-       form = request.form
-       print(form)
-   return render_template('news1.html', form=form)
-
-# @app.route('/commentInput', methods=['POST'])
-# def comment():
-#     if request.method == 'POST':
-#         form = request.form
-#
-#     return redirect(url_for(news, form=form))
-
-
-@app.route('/keywords', methods=['POST'])
-def keyword():
     if request.method == 'POST':
-        keywords = request.form['keywords']
-    return render_template('news1.html', keywords=keywords)
+        return redirect(url_for('news'), code=307) #307은 원래 전송 된대로 요청 유형을 보존
 
+@app.route('/keyword', methods=['POST'])
+def keyword():
+    #keywords = {'userID': 'cjl', 'keyword': ('sipal', 'pig')}
+    keywords = ['sibal', 'byungsin']
+    keywords.append(request.form['keyword'])
+    keywordList = ', '.join(keywords)
+    return render_template('news1.html', keywords=keywordList)
+   #return redirect(url_for('news'), code=307)
+#form 요소:ImmutableMultiDict([('userID', 'userID'), ('comment', 'zzz\r\n')])
+
+
+# @app.route('/fetchtest', methods=['POST'])
+# def fetchtest():
+#     keywords = ['test', 'byungsin']
+#     keywordList = ', '.join(keywords)
+#     return keywordList
 
 if __name__ == '__main__':
     app.run()
