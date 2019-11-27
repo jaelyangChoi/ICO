5b70ba5dd2fdf1db79f2b29fd15665451b342c4c
ChoiJaeLyang <cjl2076@naver.com>
2019-11-27 21:47:47 +0900

    ajax modal window

--- app.py
+++ app.py
@@ -1,56 +1,55 @@
-from flask import Flask, render_template, Blueprint, request, redirect,url_for
-import json
+from flask import Flask, render_template, Blueprint, request, redirect,url_for, jsonify
+
 from router.view import view_blueprint
 from block import block
 from dao import db_connection
 
 app = Flask(__name__, template_folder="templates")
 
-app.register_blueprint(view_blueprint)
+#app.register_blueprint(view_blueprint)
 # app.register_blueprint(db_connection.db_blue)
 # app.register_blueprint(block.block_blue)
 
-#@app.route('/googleCallback')
-# @app.route('/')
-# def index():
-#    return render_template('index.html')
+comments = [{'userID': 'cjl', 'comment': 'test data'},]
+keywords = ['sibal', 'byungsin']
+
+
+@app.route('/googleCallback')
+@app.route('/')
+def index():
+   return render_template('index.html')
 
 @app.route('/news', methods=["GET", "POST"])
 def news():
-    keywords = ['sibal', 'byungsin']
-    keywordList = ', '.join(keywords)
     # posts = Post.query.all()
-    comments = [{'userID': 'cjl', 'comment': 'aaaaaa\n'}]
-    if request.method == 'POST':
-            new_comment = request.form
-            comments.append(new_comment)
-        # if request.form['keyword']:
-        #     new_keyword = request.form['keywords']
-        #     keywords.append(new_keyword)
-
-    return render_template('news1.html', comments=comments, keywords=keywordList)
+    keywordStr = ', '.join(keywords)
+    return render_template('news1.html', comments=comments, keywords=keywordStr)
 
 @app.route('/commentInput', methods=['POST'])
-def comment():
-    if request.method == 'POST':
-        return redirect(url_for('news'), code=307) #307은 원래 전송 된대로 요청 유형을 보존
+def commentInput():
+    new_comment = {"userID": request.form['userID'], "comment": request.form['comment']}
+    comments.append(new_comment)
+    return redirect(url_for('news')) #, code=307 => 원래 전송 된대로 요청 유형을 보존
 
-@app.route('/keyword', methods=['POST'])
-def keyword():
-    #keywords = {'userID': 'cjl', 'keyword': ('sipal', 'pig')}
-    keywords = ['sibal', 'byungsin']
+# @app.route('/commentUpdate', methods=['POST'])
+# def commentUpdate():
+#     new_comment = {"userID":request.form['userID'],"comment":request.form['comment']}
+#     print(new_comment)
+#     comments.append(new_comment)
+#     print(comments)
+#     return jsonify(comments)
+#     #return redirect(url_for('news'))
+
+
+
+@app.route('/keywordInput', methods=['POST'])
+def keywordInput():
     keywords.append(request.form['keyword'])
-    keywordList = ', '.join(keywords)
-    return render_template('news1.html', keywords=keywordList)
-   #return redirect(url_for('news'), code=307)
+    keywordStr = ', '.join(keywords)
+    print(keywordStr)
+    return jsonify(keywordStr)
+
 #form 요소:ImmutableMultiDict([('userID', 'userID'), ('comment', 'zzz\r\n')])
-
-
-# @app.route('/fetchtest', methods=['POST'])
-# def fetchtest():
-#     keywords = ['test', 'byungsin']
-#     keywordList = ', '.join(keywords)
-#     return keywordList
 
 if __name__ == '__main__':
     app.run()
