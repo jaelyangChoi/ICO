3e1c9abc42b4d34376430c640beffca576acf31f
chowhr1231 <thddbswo14@gmail.com>
2019-12-01 20:54:32 +0900

    login test를 위한 라우터

--- router/test.py
+++ router/test.py
@@ -1,11 +1,13 @@
-from flask import Blueprint, jsonify, request
+from flask import Blueprint, jsonify, request, render_template, url_for, redirect, session
 from DB.DAO import comment
 
 from ml.model_by_word import model
+from login import googleLogin
 
 route_blue = Blueprint('route_blue', __name__)
 
 dao = comment.CommentDAO()
+gl = googleLogin.GoogleLogin()
 
 
 @route_blue.route('/test/comments/<url>')
@@ -28,3 +30,24 @@ def ml_comment():
     result = ml.predict(comment)
 
     return str(result)
+
+
+@route_blue.route('/test/google/login')
+def google_login():
+    redirect_url = gl.login()
+    return redirect_url
+
+
+@route_blue.route('/googleCallback')
+def googleCallback():
+    result = gl.google_callback()
+    session['id_token'] = result['id_token']
+    return redirect('/')
+
+
+@route_blue.route('/logout')
+def logout():
+    if 'state' in session:
+        del session['state']
+
+    return redirect(url_for('index'))
