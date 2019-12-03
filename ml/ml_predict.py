6413dedb2c0676fd149b0a60a851fcf4ef98d7d2
Hyewon Jung <ok12ok34ok56ok78@gmail.com>
2019-12-03 14:29:52 +0900

    model 추가

---
+++ ml/ml_predict.py
@@ -0,0 +1,23 @@
+from ml.ml_by_pumsa import predict_comment as pml
+from ml.model_by_word import model as wml
+
+
+class ModelCombine:
+    def total_predict(self, comment):
+        pumsa_ml = pml.CommentPredict()
+        word_ml = wml.ModelByWord()
+
+        pumsa_ml_result = pumsa_ml.predict(comment)
+        word_ml_result = word_ml.predict(comment)
+
+        if pumsa_ml_result == word_ml_result:
+            return word_ml_result
+        else:
+            if 0.6 < pumsa_ml.predict_score < 0.7:
+                return word_ml_result
+            else:
+                return pumsa_ml_result
+
+# mp=ModelCombine()
+# print(mp.total_predict("이 씨바로마"))
+
