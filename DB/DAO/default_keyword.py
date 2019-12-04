eac0f45ee90ada0191ae74d70844b3be556815e6
Nayeon <any980418@naver.com>
2019-12-05 01:03:23 +0900

    update

--- DB/DAO/default_keyword.py
+++ DB/DAO/default_keyword.py
@@ -1,17 +1,44 @@
 from DAO.user import *
+from DTO.defaultKeyword import *
 
 
 class DefaultKeywordDAO:
-
     def __init__(self):
         self.db_conn = DBConnection()
 
-    def select_keywords(self):
+    def select_all(self):
         try:
             conn = self.db_conn.get_connection()
             cursor = conn.cursor()
 
-            sql = "SELECT word FROM DefaultKeywords"
+            sql = "SELECT * FROM DefaultKeyword"
+            cursor.execute(sql)
+
+            keyword_list = []
+            for result in cursor.fetchall():
+                default_keyword = DefaultKeyword()
+                default_keyword.set_all(result)
+                keyword_list.append(default_keyword)
+
+            self.db_conn.close_db()
+            return keyword_list
+
+        except Exception as e:
+            return -1
+
+    def select_keywords(self):
+        return self.execute_sql_for_one_component_list("""SELECT keyword
+                                            FROM DefaultKeyword""")
+
+    def select_split_keywords(self):
+        return self.execute_sql_for_one_component_list("""SELECT splited_keyword
+                                            FROM DefaultKeyword""")
+
+    def execute_sql_for_one_component_list(self, sql):
+        try:
+            conn = self.db_conn.get_connection()
+            cursor = conn.cursor()
+
             cursor.execute(sql)
 
             keyword_list = []
@@ -19,7 +46,6 @@ class DefaultKeywordDAO:
                 keyword_list.append(result[0])
 
             self.db_conn.close_db()
-
             return keyword_list
 
         except Exception as e:
--- DB/DAO/user.py
+++ DB/DAO/user.py
@@ -9,31 +9,9 @@ class UserDAO(Index):
                                                FROM User
                                                WHERE id = %s""")
 
-    def is_correct_password(self, id, pw):
-        if self.execute_sql_for_one_result(id,
-                                           """SELECT password
+    def is_existing_email(self, email):
+        return self.execute_sql_for_one_result(email,
+                                           """SELECT EXISTS (
+                                           SELECT *
                                            FROM User
-                                           WHERE id = %s""") == pw:
-            return True
-        return False
-
-    def update_password(self, id, old_pw, new_pw):
-        if self.is_correct_password(id, old_pw) is True:
-            try:
-                conn = self.db_conn.get_connection()
-                cursor = conn.cursor()
-
-                sql = "UPDATE User SET password = %s WHERE id = %s"
-                cursor.execute(sql, (new_pw, id))
-                conn.commit()
-
-                self.db_conn.close_db()
-
-            except Exception as e:
-                return -1
-
-        else:
-            return 0
-
-    def insert_new_user(self):
-        pass
+                                           WHERE email = %s)""")
