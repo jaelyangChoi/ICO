90a197fbb809ba11af0865a3321d23bb960b81e7
chowhr1231 <thddbswo14@gmail.com>
2019-12-05 01:19:21 +0900

    Add: login할 때 DB와 연동해서 로직 추가
    기존의 임시 아이디로 로그인 했던 것을 실제 DB에 있는 정보를 가져와 seesion에 저장한다.
    user DTO를 추가해 DB정보를 객체로 관리하고 개인 keyword 등록 역시 DB와 연동을 했다.

--- DB/DAO/user.py
+++ DB/DAO/user.py
@@ -1,7 +1,9 @@
 from DAO.index import *
+from DTO.user import User
 
 
 class UserDAO(Index):
+    __CHECK_BY_EMAIL = "SELECT _index, id, name, email FROM user WHERE email = %s";
 
     def select_index(self, user):
         return self.execute_sql_for_one_result(user,
@@ -10,9 +12,13 @@ class UserDAO(Index):
                                                WHERE id = %s""")
 
     def is_correct_emial(self, emial):
-        if self.execute_sql_for_one_result(emial,
-                                           """SELECT *
-                                           FROM User
-                                           WHERE email = %s""") is None:
+        if self.execute_sql_for_one_result(emial, UserDAO.__CHECK_BY_EMAIL) is None:
             return -1;
         return 0
+
+    def select_by_email(self, email):
+        result = self.execute_sql_for_one_result(email, UserDAO.__CHECK_BY_EMAIL)
+        print(result)
+        user = User(result[1], result[2], result[3], result[0])
+
+        return user
