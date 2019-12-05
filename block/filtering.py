405f3a37d97f3201983c07c9771b6859a5915b9a
chowhr1231 <thddbswo14@gmail.com>
2019-12-06 02:35:03 +0900

    Fix: 댓글이 영어일 때 error 발생 해결
    letter_count로 나누는 작업이 있는데 기존의 코드에서는 한들만 letter_count가 증가하고 한글이
    아니면 letter_count가 증가하지 않아 영어만 입력이 되면 letter_count가 0이 되기 때문에 0으로
    나눌 수 없어 error가 발생하는 문제를 letter_count가 1식 증가할 수 있도록 수정했다.

--- block/filtering.py
+++ block/filtering.py
@@ -1,4 +1,4 @@
-from flask import Blueprint
+from flask import Blueprint, session
 
 from DB.DAO.comment import CommentDAO
 from DB.DAO.personal_keyword import PersonalKeywordDAO
@@ -14,8 +14,9 @@ CommentDAO = CommentDAO()
 
 # 댓글 필터링
 def filtering(comments):
+    user_info = session['info']
     # DB에서 키워드 불러옴
-    keywords = personal_keywordDB.select_keywords('cjl0701')
+    keywords = personal_keywordDB.select_keywords(user_info['id'])
     # 개인 키워드 기반 적절성 유무 판단
     if keywords:
         comments = block.privateKeywordMatch(comments, keywords)
