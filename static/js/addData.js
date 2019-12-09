5fc1e627621afc851d8eb9bfab10b7eb448036f4
ChoiJaeLyang <cjl2076@naver.com>
2019-12-09 16:15:38 +0900

    add Data 함수화/ addkeyword 이름 바꿀것

--- static/js/addData.js
+++ static/js/addData.js
@@ -4,16 +4,10 @@ function addkeyword() {
         type: 'POST',
         url: '/add_keyword',
         data: $('#keywordForm .data'), //서버로 데이터 전송시
-        success: function reloadKeywords (data) {  // string 넘어옴
-            $('#kwInputWindow').val('');
-            // $('#kwOutputWindow').val(data);
-            var keywords = '';
-            var i = 0;
-            while (i < data.length) {
-                keywords = keywords + '<input type="text" value="' + data[i] + '"><input type="button" value="삭제" onclick=""><br>';
-                i++;
-            }
-            $('#kwOutputWindow').html(keywords);
+        success: function reloadKeywords(data) {  // 키워드 리스트 넘어옴
+            $('#kwInputWindow').val(''); //입력창 초기화
+            let keywords = keywordToHTML(data);
+            $('#kwOutputWindow').html(keywords); //html로 전송
         }
     })
 }
@@ -23,17 +17,34 @@ function addComment() {
     $.ajax({
         type: 'POST',
         url: '/add_comment',
-        data: $('#commentForm .data'), //{#서버로 데이터 전송시#}
-        dataType: 'JSON',// {#서버에서 데이터 전송시#}
-        "success": function reloadComments (data) {//dict list 넘어옴
-            $('#cmInputWindow').val('');
-            var comments = '';
-            var i = 0;
-            while (i < data.length) {
-                comments = comments + '<h3>' + data[i].userID + '</h3><textarea rows="4" cols="109">' + data[i].comment + '</textarea>';
-                i++;
-            }
-            $('#cmOutputWindow').html(comments);
+        data: $('#commentForm .data'), //서버로 데이터 전송시
+        dataType: 'JSON', //서버에서 데이터 전송시
+        "success": function reloadComments(data) { //dict list 넘어옴
+            $('#cmInputWindow').val(''); //입력창 초기화
+            let comments = commentToHTML(data);
+            $('#cmOutputWindow').html(comments); //html로 전송
         }
     })
+}
+
+
+function keywordToHTML(keywordList) {
+    var keywords = '';
+    var i = 0;
+    while (i < keywordList.length) {
+        keywords = keywords + '<input type="text" value="' + keywordList[i] + '"><input type="button" value="삭제" onclick=""><br>';
+        i++;
+    }
+    return keywords
+}
+
+
+function commentToHTML(commentList) {
+    var comments = '';
+    var i = 0;
+    while (i < commentList.length) {
+        comments = comments + '<h3>' + commentList[i].userID + '</h3><textarea rows="4" cols="109">' + commentList[i].comment + '</textarea>';
+        i++;
+    }
+    return comments;
 }
